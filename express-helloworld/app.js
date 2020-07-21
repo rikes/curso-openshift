a348f8aecffa2c6e8ee2aae51b156ad091c0fd77
Henrique <henrique.santana@pdcase.com.br>
2020-07-21 00:04:51 -0300

    add package json

---
+++ express-helloworld/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/teste', function (req, res) {
+    res.send('Teste Node!');
+  });
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
